7a9d23a5f715ff3ec5c0dc4e11ad75d260c81cf0
WillisMedwell <medwellwillis@gmail.com>
2024-01-28 19:43:58 +1100

    Added online tests

--- code/Engine/include/Cameras/Orbit.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-namespace Cameras {
-    class Orbit {
-
-    };
-}
--- code/Engine/include/Model/Model.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "Model/Types.hpp"
-#include "Model/Static.hpp"
-#include "Model/Animated.hpp"
-#include "Model/Dynamic.hpp"
